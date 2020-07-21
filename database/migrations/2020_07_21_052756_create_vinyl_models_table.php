294ed5c49a28be1da4d14f7b4f13be24fb4201cf
Rajan Gurung <rajangrg1@hotmail.com>
2020-07-21 17:28:56 +1200

    vinylModel created and first migration ready

---
+++ database/migrations/2020_07_21_052756_create_vinyl_models_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVinylModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vinyls', function (Blueprint $table) {
+            $table->id();
+            $table->string('bandName');
+            $table->string('label');
+            $table->string('musicGenre');
+            $table->string('musicGenrePrimary');
+            $table->string('vinylCover')->nullable();
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+        });
+        DB::connection('mysql')->table('vinyls')->insert([
+            [
+                'bandName' => 'Led Zeppelin',
+                'label' => 'Atlantic / Swan Song / Warner',
+                'musicGenre' => 'Rock / Hard Rock',
+                'musicGenrePrimary' => 'Pop / Rock',
+                'vinylCover' => '',
+            ],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vinyl_models');
+    }
+}
